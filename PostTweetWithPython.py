81fa040a543a618f38e49b6b90336de3871f5fe3
Harshdeep <harshderp692@gmail.com>
2021-04-14 16:41:04 +0530

    Update PostTweetWithPython.py

--- PostTweetWithPython.py
+++ PostTweetWithPython.py
@@ -1,6 +1,6 @@
 #Readme file before going through the code!
-#!pip install tweepy  #uncomment this line to install a library('tweepy') that will be used to tweet on twitter.
-
+!pip install tweepy  #run this line once to install tweepy libary.
+# once installed remove the above line from your code.
 
 import tweepy
 def OAuth(accessKey, accessKeySecret, accessToken, accessTokenSecret):
@@ -12,7 +12,7 @@ def OAuth(accessKey, accessKeySecret, accessToken, accessTokenSecret):
     except Exception as e:
         return none
  
-oauth = OAuth(Your accessKey, Your accessKeySecret, Your accessToken, Your accesstokenSecret)
+oauth = OAuth(accessKey, accessKeySecret, accessToken, accesstokenSecret) #replace accesskey with your accesskey generated after creating the app, replace all with the required values and put the values in '').
 apicall = tweepy.API(oauth)
  
 apicall.update_status('Here is a sample tweet from API call program. Harshdeep')
